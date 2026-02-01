56e66c318bc06d524b5f9110728162701ceadb89
SalmaHossam <11410120206031@stud.edu.eg>
2026-02-01 10:24:35 +0200

    postgres

--- express-app/index.js
+++ express-app/index.js
@@ -1,19 +1,24 @@
+
+const { Client } = require("pg");
 const express = require("express");
 const mongoose = require("mongoose");
+const { createClient } = require("redis");
 
-const redis = require("redis");
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-const DB_USER = "root";
-const DB_PASSWORD = "example";
-const DB_PORT = 27017;
-const DB_HOST = "mongo";
+const DB_USER = 'root';
+const DB_PASSWORD = 'example';
+const DB_PORT = 5432;
+const DB_HOST = 'postgres';
+const DB_NAME = 'postgres';
+
+const connectionString = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
 const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
 
-const redisClient = redis.createClient({
+const redisClient = createClient({
   url: "redis://redis:6379"
 });
 
@@ -21,11 +26,19 @@ redisClient.connect()
   .then(() => console.log("connected to redis..."))
   .catch((err) => console.error("redis connection error:", err));
 
-mongoose
-  .connect(URI)
-  .then(() => console.log("connected to db..."))
-  .catch((err) => console.error("db connection error:", err));
+// mongoose
+//   .connect(URI)
+//   .then(() => console.log("connected to db..."))
+//   .catch((err) => console.error("db connection error:", err));
 
+const client = new Client({
+   connectionString,
+});
+client.connect()
+  .then(() => console.log("connected to postgres..."))
+  .catch((err) => console.error("postgres connection error:", err));
+
+ 
 
 app.get("/", (req, res) => {
   redisClient.set("greeting", "Hello Tresmerge from Redis!");
